5c8887093c761d3ea85450f883a124fe111a4763
anantkrd21 <89842393+anantkrd21@users.noreply.github.com>
2023-05-07 21:44:22 +0530

    added pass

--- app.js
+++ app.js
@@ -25,6 +25,9 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({
     extended: true,
 }));
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'jade');
+
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
